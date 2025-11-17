1d97f74dab2517da9d89c64e5cf5beb924b30091
yuktha2103 <mupparajuyuktha@gmail.com>
2025-11-17 09:47:15 +0530

    adding two files

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM eclipse-temurin: 11-jdk
+WORKDIR /app
+COPY . .
+RUN javac samplename.java
+CMD ["java", "samplename"]
---
+++ samplename.java
@@ -0,0 +1,6 @@
+public class samplename {
+    public static void main(String[] args){
+        String name="yuktham";
+        System.out.println("Hello i'm yuktha" + name);
+    }
+}
